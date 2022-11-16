1f4fc54c84211443b299411245674497aac97ea8
Caro-V <carolinavelasquez@MacBook-Air-de-Carolina.local>
2022-11-16 18:41:40 -0500

    Updates

--- data.js
+++ data.js
@@ -1,17 +1,45 @@
-
 import data from './data/pokemon/pokemon.js';
 
 const cadaPokemon = data.pokemon
 
-export const filtraTipos = cadaPokemon.filter (function (pokemon) {
-  return pokemon.type[0] === "grass"
-});
-
-console.log(filtraTipos)
 
 
 
-
-// export const anotherExample = () => {
-//   return 'OMG';
+// export const filtraTipos = (type,cadaPokemon) => {  
+//     const filterType= cadaPokemon.filter((element) => 
+//         element.type.includes(type)
+        
+//     );
+//     console.log(filterType);
+//     return filterType;
 // };
+
+export const filtraTipos = (type) => {
+    // console.log(type)
+    return cadaPokemon.filter((element) => {
+        return element.type.includes(type);
+    });
+}
+
+
+   export const ordenarAz = (cadaPokemon) => {
+        let arrayOrdenar = [...cadaPokemon]
+        const pokemonAz = arrayOrdenar.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1;
+              
+            }
+        });
+            return pokemonAz;
+        
+            
+    };
+// console.log(ordenarAz)
+
+
+// const filtraBuscador = (name) => {
+//     console.log(name)
+//     return cadaPokemon.filter((element) => {
+//         return element.name.includes(pikachu);
+//     });
+// }
--- main.js
+++ main.js
@@ -1,48 +1,85 @@
-
 import data from './data/pokemon/pokemon.js';
-import { filtraTipos } from './data.js';
+import { filtraTipos, ordenarAz } from './data.js';
+
 
 const cadaPokemon = data.pokemon
-const contenedor = document.getElementById("card")
+const contenedor = document.getElementById("cardList")
+const divParaModal = document.getElementById("divParaModal")
 
 
+const tarjetasPokemon = (arrPoke) => {
+    contenedor.innerHTML = '';
+    // console.log(arrPoke)
+    arrPoke.forEach((pokemon) => {
 
-
-cadaPokemon.forEach((pokemon) => {
-
-    const evoluciones = pokemon.evolution['next-evolution']
-    console.log(evoluciones)
-
-    // if (evoluciones === ("next-evolution" ||  "prev-evolution")){
-    //     console.log(evoluciones)
-    // } else {
-    //     console.log ("no tiene evolución")
-    // }
-    // console.log(evoluciones)
-
-    const tarjeta = 
-    `<div class="card">
-    <div class= "front">
-    <p class="name">${pokemon.name}</p>
-    <img src="${pokemon.img}">
-    <p class="pk_num">${pokemon.num}</p>
-    <p>Type: ${pokemon.type[0]}</p>
-    <p>Rarity: ${pokemon["pokemon-rarity"]}</p>
+        const tarjeta = document.createElement("div");
+        tarjeta.className = "card"
+        tarjeta.innerHTML =
+            `<div class= "front">
+        <p class="name">${pokemon.name}</p>
+        <img src="${pokemon.img}">
+        <p class="pk_num">${pokemon.num}</p>
+        <div>Type: ${pokemon.type.map((type) => `<span class="typeClass ${type}">${type}</span>`).join(" / ")}</div>
+        <p>Rarity: ${pokemon["pokemon-rarity"]}</p>
+        <button id="openModal" class="open" name="openModal">Learn more</button>
     </div>`
-    // <div class="back">
-    // <p>Generation: ${pokemon.generation['name']}</p><br>
-    // <p>About: ${pokemon.about}</p>
-    // </div>
+        contenedor.appendChild(tarjeta)
+    })
+};
 
-    contenedor.insertAdjacentHTML('beforebegin', tarjeta)
+(tarjetasPokemon(cadaPokemon))
+
+const modalesPokemon = (arrPoke) => {
+
+    arrPoke.forEach((pokemon) => {
+
+        const modal = document.createElement("div");
+        modal.className = "modal-container"
+        modal.id = "modal-container"
+        modal.innerHTML =
+            `<div class= "modal">
+                <p class="name">${pokemon.name}</p>
+                <img src="${pokemon.img}">
+                <p>${pokemon.about}</p>
+                <div>Attack: ${pokemon["special-attack"].map((ataque) => `<span class="ataqueClass ${ataque}"> ${ataque.name}</span>`).join(", ")}</div> 
+                <button id="closeModal" class="close">X</button>
+            </div>`
+        divParaModal.appendChild(modal)
+
+        const open = document.querySelectorAll(".open")
+        const modal_container = document.getElementById("modal-container")
+        const close = document.getElementById("closeModal")
+
+        open.forEach(arrPoke => {
+            arrPoke.addEventListener("click", function eventoModal() {
+                modal_container.classList.add("show");
+                // console.log(arrPoke)
+            })
+        })
+
+        close.addEventListener("click", () => {
+            modal_container.classList.remove("show")
+        })
+    })
+}
+(modalesPokemon(cadaPokemon))
 
 
+// document.body.addEventListener("click", (e) => {
+//     // console.log("soy un click", e.target);
+// })
+
+
+const listaTipos = document.querySelectorAll('.menuDesplegable button')
+
+listaTipos.forEach((button) => {
+    button.addEventListener("click", function eventos(e) {
+        tarjetasPokemon(filtraTipos(e.target.value))
+        
+    });
 });
 
-
-// const pokemonName = data.pokemon
-// pokemonName.forEach((pokemon) =>
-// console.log(pokemon)
-// );
-// console.log(data)
-// //})
+const buttonOrdenar = document.getElementById('buttonOrdenar')
+buttonOrdenar.addEventListener("click", function eventos (e)  {
+    tarjetasPokemon(ordenarAz (e.target.value)    
+)});
